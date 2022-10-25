e3ec0e4e316d9471fca6f6e5395110f046848392
Владислав Шиляев <schilyaev.vlad@gmail.com>
2022-10-25 14:16:30 +0300

    Add polymorphic FullName

---
+++ src/main/kotlin/domain/datatypes/FullName.kt
@@ -0,0 +1,34 @@
+package domain.datatypes
+
+import kotlinx.serialization.Serializable
+
+sealed interface FullName {
+    val value: String
+
+    @Serializable
+    class International(
+        override val value: String
+    ) : FieldData, FullName
+
+    @Serializable
+    class Russian(
+        val lastName: String,
+        val firstName: String,
+        val patronymic: String?
+    ) : FieldData, FullName {
+        override val value: String
+            get() = "$lastName $firstName $patronymic"
+
+        fun withInitials() = "${firstName.first()}.${patronymic?.let { " $it." }} $lastName"
+
+        companion object {
+            fun of(value: String): Russian? {
+                val words = value.split(' ')
+                return when (words.size) {
+                    2, 3 -> Russian(lastName = words[0], firstName = words[1], patronymic = words.getOrNull(2))
+                    else -> null
+                }
+            }
+        }
+    }
+}
