a13fe086ba79515b7ecbb32131c02b47ebdee627
Владислав Шиляев <schilyaev.vlad@gmail.com>
2022-10-26 22:57:28 +0300

    Remove space between initials

--- src/main/kotlin/domain/datatypes/RussianFullName.kt
+++ src/main/kotlin/domain/datatypes/RussianFullName.kt
@@ -9,5 +9,5 @@ data class RussianFullName(
     val patronymic: String?
 ) : FieldData {
     fun full() = "$lastName $firstName${patronymic?.let { " $it" }.orEmpty()}"
-    fun withInitials() = "${firstName.first()}.${patronymic?.first()?.let { " $it." }.orEmpty()} $lastName"
+    fun withInitials() = "${firstName.first()}.${patronymic?.first()?.let { "$it." }.orEmpty()} $lastName"
 }
