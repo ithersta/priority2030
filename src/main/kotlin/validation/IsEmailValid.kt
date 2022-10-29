10564694d235eee0da4a35f5ba5fd9c82ac89720
byzaya <75326533+byzaya@users.noreply.github.com>
2022-10-29 14:11:30 +0300

    fixed all

--- src/main/kotlin/validation/IsEmailValid.kt
+++
@@ -1,9 +0,0 @@
-package validation
-
-//насколько я помню, в первом боте мы использовали что-то готовое
-object IsEmailValid {
-    operator fun invoke(email: String): Boolean {
-        val regex = Regex(pattern = "([a-zA-Z\\d._-]+@[a-zA-Z\\d._-]+\\.[a-zA-Z\\d_-]+)")
-        return regex.matches(input = email)
-    }
-}
--- src/main/kotlin/validation/IsIndicatorValid.kt
+++ src/main/kotlin/validation/IsIndicatorValid.kt
@@ -2,7 +2,7 @@ package validation
 
 object IsIndicatorValid {
     operator fun invoke(indicator: String): Boolean {
-        val regex = Regex(pattern = "\\d*|-?")
+        val regex = Regex(pattern = "\\d+|-?")
         return regex.matches(input = indicator)
     }
 }
