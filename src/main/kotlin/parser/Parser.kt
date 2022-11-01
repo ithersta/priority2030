9fca057975efeaf269644120c834f5e1e4424926
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-11-01 12:50:11 +0300

    strings now private

--- src/main/kotlin/parser/Parser.kt
+++ src/main/kotlin/parser/Parser.kt
@@ -6,15 +6,15 @@ import java.io.IOException
 import java.util.*
 
 class Parser {
-    val mainSelect = "#container > div.sbis_ru-content_wrapper.ws-flexbox.ws-flex-column > div > div >"
-    val fullNameSelectForCom = " div:nth-child(1) > div.cCard__MainReq > div.ws-flexbox.ws-justify-content-between >" +
-            " div.cCard__MainReq-LeftSide.ws-flexbox.ws-flex-column > div.cCard__MainReq-Left >" +
+    private val mainSelect = "#container > div.sbis_ru-content_wrapper.ws-flexbox.ws-flex-column > div > div >"
+    private val fullNameSelectForCom = " div:nth-child(1) > div.cCard__MainReq > " +
+            "div.ws-flexbox.ws-justify-content-between > div.cCard__MainReq-LeftSide.ws-flexbox.ws-flex-column >" +
+            " div.cCard__MainReq-Left >" +
             " div.cCard__Director.ws-flexbox.ws-flex-column.ws-flex-wrap.ws-justify-content-start" +
             ".ws-align-items-start > div > span"
-    val fullNameSelectForIP = " div:nth-child(1) > div.cCard__MainReq.cCard__MainReq-IP > div.cCard__Name-Addr-IP >" +
-            " div.cCard__MainReq-Name > h1"
-    val data = " div.cCard__CompanyDescription > p:nth-child(5)"
-
+    private val fullNameSelectForIP = " div:nth-child(1) > div.cCard__MainReq.cCard__MainReq-IP " +
+            "> div.cCard__Name-Addr-IP > div.cCard__MainReq-Name > h1"
+    private val data = " div.cCard__CompanyDescription > p:nth-child(5)"
 
     fun takeDataUsingINN(INN: String) {
         val url = "https://sbis.ru/contragents/"
