58bfdc095891273292eceea382a5446e58cacbd9
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-11-05 20:38:19 +0300

    Update CompanyStrings.kt

--- src/main/kotlin/telegram/resources/strings/CompanyStrings.kt
+++ src/main/kotlin/telegram/resources/strings/CompanyStrings.kt
@@ -1,13 +1,13 @@
 package telegram.resources.strings
 
 object CompanyStrings {
-    const val takeInn = "Введите ИНН предприятия"
-    const val takeKpp = "Введите КПП предприятия"
-    const val takeCorrespondentAccount = "Введите корреспондентский счет предприятия"
-    const val takePaymentAccount = "Введите расчётный счет предприятия"
-    const val takeBik = "Введите БИК банка рассчетного счета предприятия"
-    const val takePostAndFio = "Введите должность и ФИО ответственного по договору от предприятия"
-    // todo: rename
-    const val takePhone = "Введите номер телефона ответственного от предприятия"
-    const val takeEmail = "Введите адрес электронной почты от предприятия"
+//     const val takeInn = "Введите ИНН предприятия"
+//     const val takeKpp = "Введите КПП предприятия"
+//     const val takeCorrespondentAccount = "Введите корреспондентский счет предприятия"
+//     const val takePaymentAccount = "Введите расчётный счет предприятия"
+//     const val takeBik = "Введите БИК банка рассчетного счета предприятия"
+//     const val takePostAndFio = "Введите должность и ФИО ответственного по договору от предприятия"
+//     // todo: rename
+//     const val takePhone = "Введите номер телефона ответственного от предприятия"
+//     const val takeEmail = "Введите адрес электронной почты от предприятия"
 }
