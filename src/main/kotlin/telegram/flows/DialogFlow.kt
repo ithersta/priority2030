f8ef791ca7bda2b733427f2fe7f1d3a60884e8dd
byzaya <75326533+byzaya@users.noreply.github.com>
2022-11-05 20:45:30 +0300

    initial commit

---
+++ src/main/kotlin/telegram/flows/DialogFlow.kt
@@ -0,0 +1,11 @@
+package telegram.flows
+
+import domain.documents.DocumentSetContext
+import telegram.entities.state.EmptyState
+
+fun .DialogFlow() {
+    state<EmptyState>{
+
+    }
+
+}
---
+++ src/main/kotlin/telegram/resourses/strings/ButtonStrings.kt
@@ -0,0 +1,19 @@
+package telegram.resourses.strings
+
+object ButtonStrings {
+
+    object ChoiceFillingDoc{
+        const val ProvisionOfServices = "Заполнить документы для оформления договора на оказание услуг с ООО или ИП\n" +
+                "(Закупка у единственного поставщика)"
+        const val DeliveryOfGoods = "Заполнить документы для оформления договора на поставку товара с ООО или ИП\n" +
+                "(Закупка у единственного поставщика)"
+        const val ConclusionGphCOntract = "Заполнить документы для заключения договора ГПХ\n" +
+                "(Закупка у единственного поставщика)"
+        const val ViewingExample = "Посмотреть примеры правильных коммерческих предложений для оформления закупки у единственного поставщика"
+    }
+    const val UploadPackageDoc = "Загрузить готовый пакет документов и отправить на проверку"
+    const val UploadDocuments = "Загрузить документы"
+    const val Send = "Отправить"
+    const val Back = "⬅️Назад"
+
+}
