83d83327a8160551c53eaf3d4080c66e91b819a2
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-11-05 20:38:03 +0300

    Update BusinessmanStrings.kt

--- src/main/kotlin/telegram/resources/strings/BusinessmanStrings.kt
+++ src/main/kotlin/telegram/resources/strings/BusinessmanStrings.kt
@@ -1,11 +1,11 @@
 package telegram.resources.strings
 
 object BusinessmanStrings {
-    const val takeInn = "Введите ИНН предпринимателя"
-    const val takeCorrespondentAccount = "Введите корреспондентский счет предпринимателя"
-    const val takePaymentAccount = "Введите расчётный счет предпринимателя"
-    const val takeBik = "Введите БИК банка рассчетного счета предпринимателя"
-    // todo: rename
-    const val takePhone = "Введите номер телефона ответственного от предпринимателя"
-    const val takeEmail = "Введите адрес электронной почты от предпринимателя"
+//     const val takeInn = "Введите ИНН предпринимателя"
+//     const val takeCorrespondentAccount = "Введите корреспондентский счет предпринимателя"
+//     const val takePaymentAccount = "Введите расчётный счет предпринимателя"
+//     const val takeBik = "Введите БИК банка рассчетного счета предпринимателя"
+//     // todo: rename
+//     const val takePhone = "Введите номер телефона ответственного от предпринимателя"
+//     const val takeEmail = "Введите адрес электронной почты от предпринимателя"
 }
