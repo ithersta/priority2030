b7aa2f8a7ea79f86b105fb2a8ce38285d91d2ea1
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-11-09 00:41:52 +0300

    1

--- src/main/kotlin/domain/datatypes/RussianFullName.kt
+++ src/main/kotlin/domain/datatypes/RussianFullName.kt
@@ -7,7 +7,7 @@ data class RussianFullName(
     val lastName: String,
     val firstName: String,
     val patronymic: String?
-) {
+) : FieldData {
     fun full() = "$lastName $firstName${patronymic?.let { " $it" }.orEmpty()}"
     fun withInitials() = "${firstName.first()}.${patronymic?.first()?.let { "$it." }.orEmpty()} $lastName"
 }
