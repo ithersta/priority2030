4cdddc1cf0faf11bdadab1e1ec13185236a2eff2
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-11-09 00:49:19 +0300

    Update ParserBik

--- src/main/kotlin/parser/ParserBik.java
+++ src/main/kotlin/parser/ParserBik.java
@@ -1,27 +1,33 @@
 package parser;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
 import java.io.IOException;
 
 public class ParserBik {
     private Document document;
 
-    void parseWebPage(String bik) {
+    public void parseWebPage(String bik) {
         try {
             String url = "https://bik-info.ru/bik_";
-            document = Jsoup.connect(url + bik + ".html").get();
+            Connection.Response response = Jsoup.connect(url + bik + ".html").timeout(10_000).execute();
+            if (response.statusCode() == 200) {
+                document = response.parse();
+                return;
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    String getCorrAccount() {
+    public String getCorrAccount() {
         String selectorCorrAccount = "body > div.container > ul:nth-child(7) > li:nth-child(2) > b";
         return document.select(selectorCorrAccount).html();
     }
 
-    String getBakName() {
+    public String getBakName() {
         String selectorNameBank = "body > div.container > ul:nth-child(7) > li:nth-child(3) > b";
         return document.select(selectorNameBank).html();
     }
