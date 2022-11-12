8b9ce0a44d4a8844399d34377dc4a269cb98c4be
Владислав Шиляев <schilyaev.vlad@gmail.com>
2022-11-12 16:53:51 +0300

    Add logging (#9)

--- src/main/kotlin/telegram/StateMachine.kt
+++ src/main/kotlin/telegram/StateMachine.kt
@@ -2,15 +2,24 @@ package telegram
 
 import com.ithersta.tgbotapi.fsm.builders.rolelessStateMachine
 import com.ithersta.tgbotapi.persistence.SqliteStateRepository
+import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
+import mu.KotlinLogging
 import telegram.entities.state.DialogState
 import telegram.entities.state.EmptyState
 import telegram.flows.documentBuildingLoop
 import telegram.flows.mainMenu
 import telegram.flows.startCommand
+import telegram.resources.strings.Strings
+
+private val logger = KotlinLogging.logger { }
 
 val stateMachine = rolelessStateMachine(
     stateRepository = SqliteStateRepository.create<DialogState>(),
-    initialState = EmptyState
+    initialState = EmptyState,
+    onException = { userId, throwable ->
+        logger.info(throwable) { userId }
+        sendTextMessage(userId, Strings.InternalError)
+    }
 ) {
     startCommand()
     mainMenu.run { invoke() }
