846f184f7e727f8f088a71754fd0a739a32e8ad6
byzaya <75326533+byzaya@users.noreply.github.com>
2022-11-28 21:57:18 +0300

    add all prompts

--- src/main/kotlin/telegram/flows/MainMenu.kt
+++ src/main/kotlin/telegram/flows/MainMenu.kt
@@ -3,10 +3,9 @@ package telegram.flows
 import com.ithersta.tgbotapi.menu.builders.menu
 import dev.inmo.tgbotapi.extensions.api.send.media.sendDocument
 import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
-import dev.inmo.tgbotapi.requests.abstracts.InputFile
+import dev.inmo.tgbotapi.requests.abstracts.MultipartFile
 import dev.inmo.tgbotapi.types.buttons.ReplyKeyboardRemove
-import dev.inmo.tgbotapi.types.files.DocumentFile
-import dev.inmo.tgbotapi.types.files.TelegramMediaFile
+import io.ktor.utils.io.streams.*
 import telegram.entities.state.DialogState
 import telegram.entities.state.EmptyState
 import telegram.entities.state.FillingProvisionOfServicesState
@@ -27,7 +26,18 @@ val mainMenu = menu<DialogState, Unit, _>(Strings.Menu.Message, EmptyState) {
     }
     button(ButtonStrings.ChoiceFillingDoc.ViewingExample) { message ->
         sendTextMessage(message.chat, Strings.comemrcialOfferPrompt(), replyMarkup = ReplyKeyboardRemove())
-        //sendDocument(message.chat, file)
+        val prompt1 = object {}::class.java.getResourceAsStream("/prompt/ArticMedia_Коммерческое_предложение_ФГАОУ_ВО_СПбПУ_Внедрение_Битрикс24.pdf")?: run {
+            return@button
+        }
+        val prompt2 = object {}::class.java.getResourceAsStream("/prompt/Asanov КП.pdf")?: run {
+            return@button
+        }
+        val prompt3 = object {}::class.java.getResourceAsStream("/prompt/КП_Политех_Nimax.pdf")?: run {
+            return@button
+        }
+        sendDocument(message.chat, MultipartFile("ArticMedia_Коммерческое_предложение_ФГАОУ_ВО_СПбПУ_Внедрение_Битрикс24.pdf") { prompt1.asInput() })
+        sendDocument(message.chat, MultipartFile("Asanov КП.pdf") { prompt2.asInput() })
+        sendDocument(message.chat, MultipartFile("КП_Политех_Nimax.pdf") { prompt3.asInput() })
         state.override { EmptyState }
     }
 }
