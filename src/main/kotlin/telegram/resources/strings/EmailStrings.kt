0c7ef24d01dd4fce5cbde249862b11aaec312c4a
byzaya <75326533+byzaya@users.noreply.github.com>
2022-12-05 23:02:45 +0300

    fix sending by email

--- src/main/kotlin/telegram/resources/strings/EmailStrings.kt
+++ src/main/kotlin/telegram/resources/strings/EmailStrings.kt
@@ -3,11 +3,9 @@ package telegram.resources.strings
 object EmailStrings {
     object ToBotUser {
         const val Subject = "Ваши сгенерированные документы"
-        const val Message = ""
     }
 
     object ToAdmin {
         fun subject(fullName: String) = "Заявка на услугу. $fullName"
-        const val Message = ""
     }
 }
