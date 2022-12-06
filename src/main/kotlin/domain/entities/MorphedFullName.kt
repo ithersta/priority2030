7e56f541944d95c03eb70812938b4cd68d109a48
Владислав Шиляев <schilyaev.vlad@gmail.com>
2022-12-06 11:59:02 +0300

    Handle no patronymic correctly

--- src/main/kotlin/domain/entities/MorphedFullName.kt
+++ src/main/kotlin/domain/entities/MorphedFullName.kt
@@ -4,10 +4,12 @@ class MorphedFullName(
     val original: String,
     val name: String,
     val surname: String,
-    val patronymic: String?,
+    val patronymic: String,
     val genitive: String
 ) {
-    private val initials get() = "${name.first()}.${patronymic?.let { "${it.first()}." }.orEmpty()}"
+    private val patronymicInitial = patronymic.firstOrNull()?.plus(".").orEmpty()
+    private val nameInitial = name.firstOrNull()?.plus(".").orEmpty()
+    private val initials get() = "$nameInitial$patronymicInitial"
     val surnameInitials get() = "$surname $initials"
     val initialsSurname get() = "$initials $surname"
 }
