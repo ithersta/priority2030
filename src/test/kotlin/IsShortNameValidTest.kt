44b4b34dd07d15e2a40453c29a31eefb0504f9ff
byzaya <75326533+byzaya@users.noreply.github.com>
2022-12-13 00:24:56 +0300

    .

--- src/test/kotlin/IsShortNameValidTest.kt
+++ src/test/kotlin/IsShortNameValidTest.kt
@@ -7,8 +7,10 @@ internal class IsShortNameValidTest {
     @org.junit.jupiter.api.Test
     operator fun invoke() {
         Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи Масайским жирафам"))
-        Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи жирафам Ротшильда в размере 20000 у.е."))
-        Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи Ангольским жирафам, предназначенным для их кормления"))
+        Assertions.assertTrue(IsShortNameValid
+            ("услуги по выплатам материальной помощи жирафам Ротшильда в размере 20000 у.е."))
+        Assertions.assertTrue(IsShortNameValid
+            ("услуги по выплатам материальной помощи Ангольским жирафам, предназначенным для их кормления"))
         Assertions.assertFalse(IsShortNameValid("услуги выплата южноафриканским жирафам"))
         Assertions.assertFalse(IsShortNameValid(""))
         Assertions.assertFalse(IsShortNameValid("1234 услуга по"))
