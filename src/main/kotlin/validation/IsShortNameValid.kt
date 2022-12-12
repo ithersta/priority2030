62318126ffd6341d006a4d911ca34864a37c8871
byzaya <75326533+byzaya@users.noreply.github.com>
2022-12-12 23:57:43 +0300

    add validation

---
+++ src/main/kotlin/validation/IsShortNameValid.kt
@@ -0,0 +1,8 @@
+package validation
+
+object IsShortNameValid {
+    operator fun invoke(cost: String): Boolean {
+        val regex = Regex(pattern = "услуги по [а-яА-Я\\d\\s]+")
+        return regex.matches(input = cost)
+    }
+}
