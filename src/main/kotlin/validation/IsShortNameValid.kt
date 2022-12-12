27afb0e386acc489d2b1a959a82ebea34d40b1e7
byzaya <75326533+byzaya@users.noreply.github.com>
2022-12-13 00:14:50 +0300

    fix some and add test

--- src/main/kotlin/validation/IsShortNameValid.kt
+++ src/main/kotlin/validation/IsShortNameValid.kt
@@ -2,7 +2,7 @@ package validation
 
 object IsShortNameValid {
     operator fun invoke(cost: String): Boolean {
-        val regex = Regex(pattern = "услуги по [а-яА-Я\\d\\s]+")
+        val regex = Regex(pattern = "услуги по [а-яА-Я\\d\\s.,]+")
         return regex.matches(input = cost)
     }
 }
---
+++ src/test/kotlin/IsShortNameValidTest.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions
+
+import validation.IsShortNameValid
+
+internal class IsShortNameValidTest {
+
+    @org.junit.jupiter.api.Test
+    operator fun invoke() {
+        Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи Масайским жирафам"))
+        Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи жирафам Ротшильда в размере 20000 у.е."))
+        Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи Ангольским жирафам, предназначенные для их кормления"))
+        Assertions.assertFalse(IsShortNameValid("услуги выплата южноафриканским жирафам"))
+        Assertions.assertFalse(IsShortNameValid(""))
+        Assertions.assertFalse(IsShortNameValid("1234 услуга по"))
+        Assertions.assertFalse(IsShortNameValid("живите, жирафы..."))
+        Assertions.assertFalse(IsShortNameValid("тест"))
+    }
+}
