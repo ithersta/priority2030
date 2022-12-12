d889c40f1d80a033a368ac43623143ee61dbbefa
byzaya <75326533+byzaya@users.noreply.github.com>
2022-12-13 00:22:31 +0300

    .

--- src/test/kotlin/IsShortNameValidTest.kt
+++ src/test/kotlin/IsShortNameValidTest.kt
@@ -8,7 +8,7 @@ internal class IsShortNameValidTest {
     operator fun invoke() {
         Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи Масайским жирафам"))
         Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи жирафам Ротшильда в размере 20000 у.е."))
-        Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи Ангольским жирафам, предназначенные для их кормления"))
+        Assertions.assertTrue(IsShortNameValid("услуги по выплатам материальной помощи Ангольским жирафам, предназначенным для их кормления"))
         Assertions.assertFalse(IsShortNameValid("услуги выплата южноафриканским жирафам"))
         Assertions.assertFalse(IsShortNameValid(""))
         Assertions.assertFalse(IsShortNameValid("1234 услуга по"))
