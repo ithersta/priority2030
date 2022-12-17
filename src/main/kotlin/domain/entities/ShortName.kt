9be44a170e86eaf9ad17635ad904014c05bef403
byzaya <75326533+byzaya@users.noreply.github.com>
2022-12-17 22:05:11 +0300

    it works

--- src/main/kotlin/domain/entities/ShortName.kt
+++ src/main/kotlin/domain/entities/ShortName.kt
@@ -2,12 +2,14 @@ package domain.entities
 
 import kotlinx.serialization.Serializable
 import validation.IsShortNameValid
+import java.util.*
 
 @Serializable
 class ShortName private constructor(
     val name: String
 ) {
     companion object {
-        fun of(name: String) = if (IsShortNameValid(name)) ShortName(name) else null
+        fun of(name: String) = ShortName(name.replaceFirstChar { it.lowercase(Locale.getDefault()) }).
+        takeIf{IsShortNameValid(it.name)}
     }
 }
