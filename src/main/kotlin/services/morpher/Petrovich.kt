7fa31e7f0def75e96a783c154d4462b73be31c6d
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-02-03 16:55:28 +0300

    Call petrovich with lock (#39)

--- src/main/kotlin/services/morpher/Petrovich.kt
+++ src/main/kotlin/services/morpher/Petrovich.kt
@@ -1,6 +1,8 @@
 package services.morpher
 
 import domain.entities.MorphedFullName
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.jruby.embed.PathType
 import org.jruby.embed.ScriptingContainer
 import org.koin.core.annotation.Single
@@ -9,14 +11,16 @@ import org.koin.core.annotation.Single
 class Petrovich {
     private val container = ScriptingContainer()
     private val receiver = container.runScriptlet(PathType.CLASSPATH, "entrypoint.rb")
+    private val mutex = Mutex()
 
-    fun morphFullName(fullName: String): MorphedFullName {
+    suspend fun morphFullName(fullName: String): MorphedFullName {
         val tokens = fullName.split(" ", limit = 3)
         val surname = tokens.getOrNull(0)
         val name = tokens.getOrNull(1)
         val patronymic = tokens.getOrNull(2)
-        val genitive =
+        val genitive = mutex.withLock {
             container.callMethod(receiver, "genitive", arrayOf<Any?>(surname, name, patronymic), String::class.java)
+        }
         return MorphedFullName(fullName, name.orEmpty(), surname.orEmpty(), patronymic.orEmpty(), genitive)
     }
 }
