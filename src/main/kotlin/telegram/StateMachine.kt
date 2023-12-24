d8eb068c7becff891b547de221fc896a1edef11e
Aleksandra <78423459+Lysenko-Aleksandra@users.noreply.github.com>
2023-12-24 12:49:16 +0300

    Improve README.md (#52)
    Co-authored-by: Lysenko-Aleksandra <Lysenko-Aleksandra@users.noreply.github.com>

--- src/main/kotlin/telegram/StateMachine.kt
+++ src/main/kotlin/telegram/StateMachine.kt
@@ -3,14 +3,10 @@ package telegram
 import com.ithersta.tgbotapi.commands.cancelCommand
 import com.ithersta.tgbotapi.commands.fallback
 import com.ithersta.tgbotapi.fsm.builders.rolelessStateMachine
-import com.ithersta.tgbotapi.persistence.SqliteStateRepository
-import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
 import dev.inmo.tgbotapi.types.UserId
-import mu.KotlinLogging
 import telegram.entities.state.DialogState
 import telegram.entities.state.EmptyState
 import telegram.flows.*
-import telegram.resources.strings.Strings
 
 val stateMachine = rolelessStateMachine<DialogState, UserId>(
     initialState = EmptyState,
